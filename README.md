79b76b25ab2750f38a099deb83ace72f9dbc330a
Prathyusha <90699228+P-Peddireddy@users.noreply.github.com>
2022-10-14 20:40:26 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# AllAPI
-echo "# AllAPI" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/P-Peddireddy/AllAPI.git
-git push -u origin main
+# EmployeeApplication
+ 
---
+++ ViewModels/AllAPiListModel.cs
@@ -0,0 +1,34 @@
+﻿using EmployeeApplication.Models;
+using EmployeeApplication.Services;
+using MvvmHelpers;
+using MvvmHelpers.Commands;
+
+namespace EmployeeApplication.ViewModels
+{
+    //https://devblogs.microsoft.com/dotnet/introducing-the-net-maui-community-toolkit-preview/#what-to-expect-in-net-maui-toolkit
+
+    internal class AllAPiListModel
+    {
+        public IAllAPI<AllApi> DataStore => DependencyService.Get<IAllAPI<AllApi>>();
+        public ObservableRangeCollection<AllApi> Employees { get; set; }
+        public AsyncCommand PageAppearingCommand { get; }
+
+        public AllAPiListModel()
+        {
+            Employees = new ObservableRangeCollection<AllApi>();
+            PageAppearingCommand = new AsyncCommand(PageAppearing);
+        }
+
+        public async Task Refresh()
+        {
+            var employees = await DataStore.GetEmployeesAsync();
+
+            Employees.AddRange(employees);
+        }
+
+        async Task PageAppearing()
+        {
+            await Refresh();
+        }
+    }
+}
